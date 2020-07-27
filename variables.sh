9f46ed3be4997a8c5142fd7e579aefbbe176a826
Ravindra Bhargava <meetravi007@gmail.com>
2020-07-27 19:39:11 +0800

    v1.0

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TF_VAR_project=$(gcloud config get-value project)
 export TF_VAR_region="us-central1"
-export TF_VAR_cluster_name="my-terraform-gke-cluster"
+export TF_VAR_cluster_name="my-terraform-gke-cluster
